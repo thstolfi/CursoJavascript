ab82c5acde443721751c0d95f2f983bd154c0b49
Theilor Stolfi <thstolfi@gmail.com>
2023-12-13 12:23:42 -0300

    new exercise

---
+++ ex011.js
@@ -0,0 +1,11 @@
+var idade = 19;
+if(idade < 16){
+    console.log("Não vota");
+}else{
+    if(idade > 16 && idade < 18){
+        console.log("Voto opcional")
+    }else{
+        console.log("Vota")
+    }
+    
+}
