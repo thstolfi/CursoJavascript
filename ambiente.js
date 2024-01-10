557110b03c6c930dfbe1f0244a28b9da876acedd
Theilor Stolfi <thstolfi@gmail.com>
2024-01-10 13:05:48 -0300

    new exercise4

--- ambiente.js
+++
@@ -1,5 +0,0 @@
-let num = [5, 8, 4]
-num[3] = 6 //JS insere na posição mesmo que ela não exista
-num.push(7) //Insere na última posição
-num.sort() //Organiza os elementos em ordem crescente
-num.indexOf(7) //Procura no vetor onde está o valor 7   
