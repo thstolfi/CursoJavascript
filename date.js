aa7ad9efb037fbf8334bcaafb1a684c1abc04a3e
Theilor Stolfi <thstolfi@gmail.com>
2024-01-26 14:07:22 -0300

    new exercise

---
+++ date.js
@@ -0,0 +1,2 @@
+const data = new Date(2019, 3, 20, 15, 14, 59);
+console.log(data.toString())
---
+++ return.js
@@ -0,0 +1,10 @@
+let a = 2
+let b = 3
+
+function teste(x, y){
+    var c = x + y
+    return c;
+}
+
+let resultado = teste(a, b)
+console.log(resultado)
